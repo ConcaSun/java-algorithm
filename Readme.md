926ad00fc07e7d06d8dc625810b3a3daa41e44da
suntg <suntg@usoftchina.com>
2018-10-10 13:49:43 +0800

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Java Algorithm
+Java 算法题目实践项目，针对常见的算法题目进行实践编写
+
+领扣题库网 https://leetcode-cn.com/problemset/all/
---
+++ src/main/java/com/usoft/suntg/algorithm/JavaAlgorithmApplication.java
@@ -0,0 +1,12 @@
+package com.usoft.suntg.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAlgorithmApplication.class, args);
+	}
+}
