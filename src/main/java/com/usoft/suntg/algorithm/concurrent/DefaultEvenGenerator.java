ba63b8a167e276f8e511c921df50956f2d38e3e1
suntg <suntg@usoftchina.com>
2019-04-21 23:21:15 +0800

    feat: 增加synchronized和注释

--- src/main/java/com/usoft/suntg/algorithm/concurrent/DefaultEvenGenerator.java
+++ src/main/java/com/usoft/suntg/algorithm/concurrent/DefaultEvenGenerator.java
@@ -9,7 +9,12 @@ public class DefaultEvenGenerator extends EvenGenerator {
 
     private int currentEvenValue = 0;
 
-    public int next() {
+    /**
+     * 如果加了synnchronized 关键字，方法被加锁，不会同时被多个线程调用，所以一直会是偶数
+     * 如果不加，因为i++不是原子性，所以会出现公用资源，出现奇数
+     * @return
+     */
+    public synchronized int next() {
         currentEvenValue ++;
         yield();
         currentEvenValue ++;
